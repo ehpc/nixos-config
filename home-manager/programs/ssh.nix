33bd2e409725ee20c818581247fd7df328592608
ehpc <ehpc@ehpc.io>
2025-12-18 20:08:25 +0300

    fix ssh config permissions

--- home-manager/programs/ssh.nix
+++ home-manager/programs/ssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.ssh = {
     enable = true;
@@ -11,4 +11,16 @@
       };
     };
   };
+  home.activation.fixSshConfigPermissions = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run mkdir -p "$HOME/.ssh"
+    run chmod 700 "$HOME/.ssh"
+
+    if [ -L "$HOME/.ssh/config" ]; then
+      src="$(readlink -f "$HOME/.ssh/config")"
+      if [ -f "$src" ]; then
+        run rm -f "$HOME/.ssh/config"
+        run install -m600 "$src" "$HOME/.ssh/config"
+      fi
+    fi
+  '';
 }
