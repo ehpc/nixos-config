9ba4210c542f0d3750fed17bd49983fd344a491f
ehpc <ehpc@ehpc.io>
2025-12-29 23:06:09 +0300

    fix btop gpu name

--- home-manager/programs/btop/default.nix
+++ home-manager/programs/btop/default.nix
@@ -14,6 +14,7 @@ in
       color_theme = "catppuccin-mocha";
       shown_boxes = "cpu gpu0 mem net proc";
       proc_aggregate = true;
+      custom_gpu_name0 = "Radeon RX 6600 Fighter 8GB";
     };
     themes = {
       catppuccin-mocha = builtins.readFile ./${themeFile};
